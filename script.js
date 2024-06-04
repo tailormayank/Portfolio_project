2d8bf4077a18cd6a59f37c9d68f389af90dac770
mayank_tailor <tailormayank6953@gamil.com>
2024-06-05 00:39:56 +0530

    Navigation Menu for both mobile/desktop complete

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menuLinks");
+    const icon = document.querySelector(".hamburgerIcon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
